85134d05229b97c5d210771a324e1da2fdade9cf
Enrique Martínez Añon <entique.ma@gmail.com>
2020-02-18 20:03:29 +0100

    segundo commit kata monedas

--- spec/Monedas_spec.rb
+++ spec/Monedas_spec.rb
@@ -1,60 +1,111 @@
 =begin
-  -Necesitamos un servicio al que pasarle una cantidad de dinero y que devuelva la cantidad mínima de monedas necesarias para sumar tal cantidad. 
+  -Necesxitamos un servicio al que pasarle una cantidad de dinero y que devuelva la cantidad mínima de monedas necesarias para sumar tal cantidad. 
   -Debemos usar el euro, considerando solo las monedas de uso general, es decir, monedas de euro (1 € y 2 €) y monedas de centimos (50cent, 20cent, 10cent, 5cent, 2cent, 1cent). 
 =end
 
 
 describe 'Monedas' do
   
-  it 'should return one euro if coin one euro' do
-    monedas = Monedas.new
+  it 'debería devolver 1 moneda de 1 € si nos dan 1 €' do
     euro = 1
+    cent = 0
 
-    result = monedas.minimum_coins(euro)
+    result = cantidad_minima(euro, cent)
 
-    expect(result).to eq("1 moneda de 1 euro")
+    expect(result).to eq("1 moneda de 1 €")
   end
 
-  it 'should return two euro if coin two euro' do
-    monedas = Monedas.new
+  it 'debería devolver 1 moneda de 2 € si nos dan 2 €' do
     euro = 2
+    cent = 0
 
-    result = monedas.minimum_coins(euro)
+    result = cantidad_minima(euro, cent)
 
-    expect(result).to eq("1 moneda de 2 euros")
+    expect(result).to eq("1 moneda de 2 €")
   end
 
-  it 'should return three euro if coin three euro' do
-    monedas = Monedas.new
+  it 'debería devolver 1 moneda de 2 € y 1 moneda de 1 € si nos dan 3€' do
     euro = 3
+    cent = 0
 
-    result = monedas.minimum_coins(euro)
+    result = cantidad_minima(euro, cent)
 
-    expect(result).to eq("1 moneda de 2 euros 1 moneda de 1 euro")
+    expect(result).to eq("1 moneda de 2 € y 1 moneda de 1 €")
+  end
+
+  it 'debería devolver 2 monedas de 2 € si nos dan 4€' do
+    euro = 4
+    cent = 0
+
+    result = cantidad_minima(euro, cent)
+
+    expect(result).to eq("2 monedas de 2 €")
+  end
+
+  it 'debería devolver 2 monedas de 2 € y 1 moneda de 1 € si nos dan 5€' do
+    euro = 5
+    cent = 0
+
+    result = cantidad_minima(euro, cent)
+
+    expect(result).to eq("2 monedas de 2 € y 1 moneda de 1 €")
+  end
+
+  it 'debería devolver 1 moneda de 1 centimo si nos dan 1 centimo' do
+    euro = 0.50
+
+    result = cantidad_minima_de_euros(euro)
+
+    expect(result).to eq("1 moneda de 50 centimos")
   end
 
 end
 
-class Monedas
-  MONEDAS =  {1 => "1 euro", 2 => "2 euros"}
+def cantidad_minima(euro, cent)
+  cantidad_minima_de_euros(euro)
+end
 
-  def minimum_coins(euro)
-    if MONEDAS.key?(euro)
-      one_of(MONEDAS[euro])
-    elsif euro == 3
-      "#{two_euros} #{one_euro}"
+def cantidad_minima_de_euros(euro)
+  @euro = euro
+  return menos_de_cuatro_euros if euro < 4
+  return varias_monedas_de_dos_euros if euro.even?
+  return "#{varias_monedas_de_dos_euros} y #{un_euro}" if euro.odd?
+end
+
+MONEDAS_EURO = { 2 => "2 €", 1 => "1 €", 0.50 => "50 centimos"}
+
+def menos_de_cuatro_euros
+  devolucion = ""
+  MONEDAS_EURO.each do |moneda, valor|
+    while @euro >= moneda
+      @euro -= moneda
+      devolucion += "1 moneda de #{valor}"
+    end
+    if @euro > moneda
+      @euro -= moneda
+      devolucion += "#{dos_euros} y "
     end
   end
+  devolucion
+end
 
-  def one_of(coin)
-    "1 moneda de #{coin}"
-  end
+def dos_euros
+  "1 moneda de 2 €"
+end
 
-  def one_euro
-    "1 moneda de 1 euro"
-  end
+def un_euro
+  "1 moneda de 1 €"
+end
 
-  def two_euros
-    "1 moneda de 2 euros"
-  end
+def varias_monedas_de_dos_euros
+  "#{cantidad} monedas de 2 €"
+end
+
+
+def cantidad
+  @euro / 2
+end
+
+def cantidad_minima_de_centimos(cent)
+  "1 moneda de 1 centimo"
 end
--- src/fizzbuzz.rb
+++ src/fizzbuzz.rb
@@ -2,37 +2,41 @@ class FizzBuzz
 
   def fizz_buzz_game(number)
     @number = number
-    return "FizzBuzz" if is_divisible_by_fifteen? && if_the_number_ends_in_five == false 
-    return "Buzz"     if is_divisible_by_five? 
+    return "FizzBuzz" if is_divisible_by_fifteen? && if_the_number_does_not_end_in_five
+    return "Buzz"     if is_divisible_by_five?
     return "Fizz"     if is_divisible_by_three? || if_the_number_ends_in_three
     number
   end
 
 private
+  THREE, FIVE, FIFTEEN = 3, 5, 15
 
   def is_divisible_by_three?
-    is_divisible_by(@number, 3)
+    is_divisible_by?(THREE)
   end
 
   def is_divisible_by_five?
-    is_divisible_by(@number, 5)
+    is_divisible_by?(FIVE)
   end
 
   def is_divisible_by_fifteen?
-    is_divisible_by(@number, 15)
+    is_divisible_by?(FIFTEEN)
   end
 
-  def is_divisible_by(numbers, divisors)
-    @divisors = divisors
-    @number % @divisors == 0
+  def is_divisible_by?(divisor)
+    @number % divisor == 0
   end
 
   def if_the_number_ends_in_three
-    @number.digits[0] == 3
+    the_last_digit == THREE
   end
 
-  def if_the_number_ends_in_five
-    @number.digits[0] == 5
+  def if_the_number_does_not_end_in_five
+    the_last_digit != FIVE
+  end
+
+  def the_last_digit
+    @number.digits[0]
   end
 
 end
