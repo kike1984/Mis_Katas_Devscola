31eca96ff4bf45566cfecb7afbdd74762d37a161
Enrique Martínez Añon <entique.ma@gmail.com>
2020-02-21 18:38:19 +0100

    fallo al devolver 30 centimos

--- spec/Monedas_spec.rb
+++ spec/Monedas_spec.rb
@@ -8,104 +8,109 @@ describe 'Monedas' do
   
   it 'debería devolver 1 moneda de 1 € si nos dan 1 €' do
     euro = 1
-    cent = 0
 
-    result = cantidad_minima(euro, cent)
+    result = cantidad_minima(euro)
 
-    expect(result).to eq("1 moneda de 1 €")
+    expect(result).to eq("1 de 1 €")
   end
 
   it 'debería devolver 1 moneda de 2 € si nos dan 2 €' do
     euro = 2
-    cent = 0
 
-    result = cantidad_minima(euro, cent)
+    result = cantidad_minima(euro)
 
-    expect(result).to eq("1 moneda de 2 €")
+    expect(result).to eq("1 de 2 €")
   end
 
   it 'debería devolver 1 moneda de 2 € y 1 moneda de 1 € si nos dan 3€' do
     euro = 3
-    cent = 0
 
-    result = cantidad_minima(euro, cent)
+    result = cantidad_minima(euro)
 
-    expect(result).to eq("1 moneda de 2 € y 1 moneda de 1 €")
+    expect(result).to eq("1 de 2 €, 1 de 1 €")
   end
 
   it 'debería devolver 2 monedas de 2 € si nos dan 4€' do
     euro = 4
-    cent = 0
 
-    result = cantidad_minima(euro, cent)
+    result = cantidad_minima(euro)
 
-    expect(result).to eq("2 monedas de 2 €")
+    expect(result).to eq("2 de 2 €")
   end
 
   it 'debería devolver 2 monedas de 2 € y 1 moneda de 1 € si nos dan 5€' do
     euro = 5
-    cent = 0
 
-    result = cantidad_minima(euro, cent)
+    result = cantidad_minima(euro)
 
-    expect(result).to eq("2 monedas de 2 € y 1 moneda de 1 €")
+    expect(result).to eq("2 de 2 €, 1 de 1 €")
   end
 
-  it 'debería devolver 1 moneda de 1 centimo si nos dan 1 centimo' do
+  it 'debería devolver 1 moneda de 50 centimo si nos dan 50 centimos' do
     euro = 0.50
 
-    result = cantidad_minima_de_euros(euro)
+    result = cantidad_minima(euro)
 
-    expect(result).to eq("1 moneda de 50 centimos")
+    expect(result).to eq("1 de 50 centimos")
+  end
+
+  it 'debería devolver 2 moneda de 20 centimo si nos dan 40 centimos' do
+    euro = 0.40
+
+    result = cantidad_minima(euro)
+
+    expect(result).to eq("2 de 20 centimos")
+  end
+
+  it 'debería devolver 1 moneda de 20 centimo y 1 moneda de 10 centimos si nos dan 30 centimos' do
+    euro = 0.30
+
+    result = cantidad_minima(euro)
+
+    expect(result).to eq("1 de 20 centimos, 1 de 10 centimos")
   end
 
 end
 
-def cantidad_minima(euro, cent)
-  cantidad_minima_de_euros(euro)
-end
-
-def cantidad_minima_de_euros(euro)
+def cantidad_minima(euro)
   @euro = euro
-  return menos_de_cuatro_euros if euro < 4
-  return varias_monedas_de_dos_euros if euro.even?
-  return "#{varias_monedas_de_dos_euros} y #{un_euro}" if euro.odd?
+  cantidad_minima_de_euros
 end
 
-MONEDAS_EURO = { 2 => "2 €", 1 => "1 €", 0.50 => "50 centimos"}
-
-def menos_de_cuatro_euros
-  devolucion = ""
-  MONEDAS_EURO.each do |moneda, valor|
-    while @euro >= moneda
-      @euro -= moneda
-      devolucion += "1 moneda de #{valor}"
-    end
-    if @euro > moneda
-      @euro -= moneda
-      devolucion += "#{dos_euros} y "
+def cantidad_minima_de_euros
+  euro = euro_con_centimos
+  devolucion = []
+  MONEDAS.each do |moneda, valor|
+    if euro > moneda * 2
+      euro -= moneda * cantidad
+      devolucion << "#{cantidad} de #{valor}"
+      puts euro
+    elsif euro == moneda * 2
+      devolucion << "2 de #{valor}"
+      euro -= moneda * 2
+    elsif euro >= moneda
+      devolucion << "1 de #{valor}"
+      euro -= moneda
     end
   end
-  devolucion
+  devolucion.join(', ')
 end
 
-def dos_euros
-  "1 moneda de 2 €"
-end
+MONEDAS = {
+  2 => "2 €",
+  1 => "1 €",
+  0.50 => "50 centimos",
+  0.20 => "20 centimos", 
+  0.10 => "10 centimos",
+  0.05 => "5 centimos",
+  0.02 => "2 centimos",
+  0.01 => "1 centimo"
+}
 
-def un_euro
-  "1 moneda de 1 €"
+def euro_con_centimos
+  @euro.to_f
 end
-
-def varias_monedas_de_dos_euros
-  "#{cantidad} monedas de 2 €"
-end
-
 
 def cantidad
-  @euro / 2
-end
-
-def cantidad_minima_de_centimos(cent)
-  "1 moneda de 1 centimo"
+  @euro.to_i / 2
 end
